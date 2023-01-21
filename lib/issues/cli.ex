8232c09de7cda0abadf8433a46e80bdfc5671cbf
Kwanghoon Choi <lazyswamp@gmail.com>
2023-01-21 23:31:32 +0900

    13.10: take n items

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -28,6 +28,13 @@
     Issues.GithubIssues.fetch(user, project)
     |> Issues.GithubIssues.decode_response()
     |> sort_into_descending_order()
+    |> last(_count)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
   end
 
   def sort_into_descending_order(list_of_issues) do
